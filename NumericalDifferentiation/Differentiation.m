9cf87921cc44f8eaa2c5808552085956bfd7557f
YuhanDing <jinliang98@hotmail.com>
2025-04-02 13:18:24 -0500

    Update Differentiation.m

--- NumericalDifferentiation/Differentiation.m
+++ NumericalDifferentiation/Differentiation.m
@@ -12,16 +12,16 @@
 % Number of trials
 N = 8;
 
-%% Result of symmetric difference
-disp('Result of symmetric difference:')
+%% Result of forward difference
+disp('Result of forward difference:')
 for n=0:N
     x = 10^(-n);
      Dhf = (atan(sqrt(2)+x)-atan(sqrt(2)))/x;
     fprintf('h = %f, the value is %e\n',x,Dhf)
 end
 
-%% Result of forward difference
-disp('Result of forward difference:')
+%% Result of symmetric difference
+disp('Result of symmetric difference:')
 for n=0:N
     x = 10^(-n);
     Dhf = (atan(sqrt(2)+x)-atan(sqrt(2)-x))/2/x;
